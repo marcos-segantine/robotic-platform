ee3bec7419618c059132d10787a4c49f1368593a
Marcos Segantine <msegantine8@gmail.com>
2024-07-17 10:28:33 -0300

    feat: creating login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 
 import { AuthOptionsComponent } from './features/auth/pages/auth-options/auth-options.component';
+import { LoginComponent } from './features/auth/pages/login/login.component';
 
 export const routes: Routes = [
     {
         path: "",
         component: AuthOptionsComponent
-    }
+    },
+    {
+        path: "login",
+        component: LoginComponent
+    },
 ];
--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { InputComponent } from '../../components/input/input.component';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [InputComponent, ButtonComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
