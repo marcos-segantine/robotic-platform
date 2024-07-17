8eb64fa1b2f3000ba5fce5217724f4c0b6ff2516
Marcos Segantine <msegantine8@gmail.com>
2024-07-17 11:08:36 -0300

    feat: creating maintenance page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { AuthOptionsComponent } from './features/auth/pages/auth-options/auth-options.component';
 import { LoginComponent } from './features/auth/pages/login/login.component';
+import { MaintenanceComponent } from './core/components/pages/maintenance/maintenance.component';
 
 export const routes: Routes = [
     {
@@ -11,5 +12,17 @@ export const routes: Routes = [
     {
         path: "login",
         component: LoginComponent
+    },
+    {
+        path: "professor-auth",
+        component: MaintenanceComponent
+    },
+    {
+        path: "matrix-auth",
+        component: MaintenanceComponent
+    },
+    {
+        path: "institutional-auth",
+        component: MaintenanceComponent
     },
 ];
---
+++ src/app/core/components/pages/maintenance/maintenance.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
+
+@Component({
+  selector: 'app-maintenance',
+  standalone: true,
+  imports: [ButtonComponent],
+  templateUrl: './maintenance.component.html',
+  styleUrl: './maintenance.component.scss'
+})
+export class MaintenanceComponent {
+
+}
