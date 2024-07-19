d08f9c0acd0fd1b53d998cb85c71309097682794
Marcos Segantine <msegantine8@gmail.com>
2024-07-19 19:18:52 -0300

    feat: improving ui from login page, adding flag and button

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterLinkWithHref } from '@angular/router';
 
 import { InputComponent } from '../../components/input/input.component';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
@@ -7,7 +8,7 @@ import { ButtonType } from '../../enum/button-type-enum';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [InputComponent, ButtonComponent],
+  imports: [InputComponent, ButtonComponent, RouterLink, RouterLinkActive, RouterLinkWithHref],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
