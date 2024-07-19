aae8b0c4ad3b7706e481f73a7a1debb584c7eb1f
Marcos Segantine <msegantine8@gmail.com>
2024-07-19 18:13:20 -0300

    feat: adding footer component

---
+++ src/app/core/components/layout/footer/footer.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterOutlet, RouterLinkActive],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent implements OnInit {
+  footerMode: "primary" | "secondary" = "primary";
+
+  ngOnInit(): void {
+    const currentPath = document.location.href.split('/').at(-1);
+
+    if (currentPath === "" || currentPath === "login") this.footerMode = "primary"
+    else this.footerMode = "secondary"
+  }
+}
