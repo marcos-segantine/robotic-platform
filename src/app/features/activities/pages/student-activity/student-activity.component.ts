a550a212f22818a66c93ebf71cb71f8726c3b28c
Marcos Segantine <msegantine8@gmail.com>
2024-07-28 14:52:43 -0300

    feat: creating shared component

--- src/app/features/activities/pages/student-activity/student-activity.component.ts
+++ src/app/features/activities/pages/student-activity/student-activity.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { TrailActivitiesComponent } from '../../components/trail-activities/trail-activities.component';
+import { LearningProgressComponent } from '../../../../shared/components/learning-progress/learning-progress.component';
 
 @Component({
   selector: 'app-student-activity',
   standalone: true,
-  imports: [TrailActivitiesComponent],
+  imports: [TrailActivitiesComponent, LearningProgressComponent],
   templateUrl: './student-activity.component.html',
   styleUrl: './student-activity.component.scss'
 })
---
+++ src/app/shared/components/learning-progress/learning-progress.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-learning-progress',
+  standalone: true,
+  imports: [],
+  templateUrl: './learning-progress.component.html',
+  styleUrl: './learning-progress.component.scss'
+})
+export class LearningProgressComponent {
+
+}
