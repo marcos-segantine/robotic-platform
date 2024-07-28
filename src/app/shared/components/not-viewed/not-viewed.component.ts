45c30f9e75ba54aa93adcbad9ba74b1f300a45bc
Marcos Segantine <msegantine8@gmail.com>
2024-07-28 16:03:57 -0300

    feat: creating shared component

---
+++ src/app/shared/components/not-viewed/not-viewed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-viewed',
+  standalone: true,
+  imports: [],
+  templateUrl: './not-viewed.component.html',
+  styleUrl: './not-viewed.component.scss'
+})
+export class NotViewedComponent {
+
+}
