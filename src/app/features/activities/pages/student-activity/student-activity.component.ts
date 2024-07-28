c6f45baab366f3b580d719f4fc57f0cf9a542873
Marcos Segantine <msegantine8@gmail.com>
2024-07-28 17:21:20 -0300

    feat: adding content to home page to student

--- src/app/features/activities/pages/student-activity/student-activity.component.ts
+++ src/app/features/activities/pages/student-activity/student-activity.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { TrailActivitiesComponent } from '../../components/trail-activities/trail-activities.component';
 import { LearningProgressComponent } from '../../../../shared/components/learning-progress/learning-progress.component';
+import { NotViewedComponent } from '../../../../shared/components/not-viewed/not-viewed.component';
 
 @Component({
   selector: 'app-student-activity',
   standalone: true,
-  imports: [TrailActivitiesComponent, LearningProgressComponent],
+  imports: [TrailActivitiesComponent, LearningProgressComponent, NotViewedComponent],
   templateUrl: './student-activity.component.html',
   styleUrl: './student-activity.component.scss'
 })
--- src/app/features/dashboard/pages/student-dashboard/student-dashboard.component.ts
+++ src/app/features/dashboard/pages/student-dashboard/student-dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { LearningProgressComponent } from '../../../../shared/components/learning-progress/learning-progress.component';
+import { NotViewedComponent } from '../../../../shared/components/not-viewed/not-viewed.component';
+
 @Component({
   selector: 'app-student-dashboard',
   standalone: true,
-  imports: [],
+  imports: [LearningProgressComponent, NotViewedComponent],
   templateUrl: './student-dashboard.component.html',
   styleUrl: './student-dashboard.component.scss'
 })
