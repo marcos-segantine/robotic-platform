1e063dc28cdb300c16091b33b2860a2dfbbeb03c
Marcos Segantine <msegantine8@gmail.com>
2024-07-29 22:21:22 -0300

    feat: adding lessons page for student

--- src/app/features/activities/pages/student-lessons/student-lessons.component.ts
+++ src/app/features/activities/pages/student-lessons/student-lessons.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { LessonLinkComponent } from '../../components/lesson-link/lesson-link.component';
+import { RankingComponent } from '../../../award/ranking/ranking.component';
 
 @Component({
   selector: 'app-student-lessons',
   standalone: true,
-  imports: [LessonLinkComponent],
+  imports: [LessonLinkComponent, RankingComponent],
   templateUrl: './student-lessons.component.html',
   styleUrl: './student-lessons.component.scss'
 })
