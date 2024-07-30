82e8c37d16ce76d9e1eb22edd115689ee935637c
Marcos Segantine <msegantine8@gmail.com>
2024-07-30 00:11:38 -0300

    feat: creating score lesson component

--- src/app/features/activities/components/lesson-link/lesson-link.component.ts
+++ src/app/features/activities/components/lesson-link/lesson-link.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
 
 import { ButtonType } from '../../../auth/enum/button-type-enum';
+import { LessonScoreComponent } from "../lesson-score/lesson-score.component";
 
 @Component({
   selector: 'app-lesson-link',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, LessonScoreComponent],
   templateUrl: './lesson-link.component.html',
   styleUrl: './lesson-link.component.scss'
 })
---
+++ src/app/features/activities/components/lesson-score/lesson-score.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson-score',
+  standalone: true,
+  imports: [],
+  templateUrl: './lesson-score.component.html',
+  styleUrl: './lesson-score.component.scss'
+})
+export class LessonScoreComponent {
+
+}
