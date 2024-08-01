1df2ee7485b87125014308d2bce04ff64df9022a
Marcos Segantine <msegantine8@gmail.com>
2024-08-01 14:12:58 -0300

    feat: adding lib to dashboard, creating random chart

--- src/app/features/dashboard/pages/student-dashboard/student-dashboard.component.ts
+++ src/app/features/dashboard/pages/student-dashboard/student-dashboard.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { LearningProgressComponent } from '../../../../shared/components/learning-progress/learning-progress.component';
 import { NotViewedComponent } from '../../../../shared/components/not-viewed/not-viewed.component';
+import { Chart, ChartItem, registerables } from 'chart.js';
+
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-student-dashboard',
@@ -10,6 +13,70 @@ import { NotViewedComponent } from '../../../../shared/components/not-viewed/not
   templateUrl: './student-dashboard.component.html',
   styleUrl: './student-dashboard.component.scss'
 })
-export class StudentDashboardComponent {
+export class StudentDashboardComponent implements OnInit {
+  chart: Chart | null = null;
 
+  ngOnInit(): void {
+    const ctx = document.getElementById('chart') as ChartItem;
+
+    this.chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['January',
+          'February',
+          'March',
+          'April',
+          'May'],
+        datasets: [{
+          data: [5000, 7500, 8000, 6000, 9000],
+          tension: 0.4,
+          pointRadius: 0,
+          backgroundColor: 'rgba(255, 181, 0, .5)',
+          borderColor: '#FFB500',
+          borderWidth: 2,
+          fill: 'start',
+        }]
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: false
+          },
+          tooltip: {
+            padding: 10,
+            titleMarginBottom: 15,
+            enabled: true,
+            intersect: false,
+            mode: 'nearest',
+            callbacks: {
+              label: function (context) {
+                let label = context.dataset.label || '';
+
+                if (label) {
+                  label += ': ';
+                }
+                if (context.parsed.y !== null) {
+                  label += new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(context.parsed.y);
+                }
+                return label;
+              }
+            }
+          }
+        },
+        responsive: true,
+        scales: {
+          x: {
+            ticks: {
+              color: "#808080"
+            }
+          },
+          y: {
+            ticks: {
+              color: "#808080",
+            }
+          },
+        },
+      },
+    });
+  }
 }
