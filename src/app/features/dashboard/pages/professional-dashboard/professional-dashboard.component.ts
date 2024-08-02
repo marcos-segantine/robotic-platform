cc97ebe93d0cd9c6a6ba278c0352d59a3a7caa51
Marcos Segantine <msegantine8@gmail.com>
2024-08-02 14:38:54 -0300

    feat: creating the frame from professional dashdoard

--- src/app/features/dashboard/pages/professional-dashboard/professional-dashboard.component.ts
+++ src/app/features/dashboard/pages/professional-dashboard/professional-dashboard.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
+import { RankingComponent } from '../../../award/ranking/ranking.component';
+
 @Component({
   selector: 'app-professional-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RankingComponent],
   templateUrl: './professional-dashboard.component.html',
   styleUrl: './professional-dashboard.component.scss'
 })
 export class ProfessionalDashboardComponent {
-
+  
 }
