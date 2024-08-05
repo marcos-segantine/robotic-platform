b3490406d0338f6b34e072e51aac125959d2ba38
Marcos Segantine <msegantine8@gmail.com>
2024-08-05 14:15:18 -0300

    style: adding additional styles for input component

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -10,6 +10,7 @@ import { Component, Input } from '@angular/core';
 export class InputComponent {
   @Input({ required: true }) size: "all" | "bigger" | "medium" | "small" = "medium";
   @Input({ required: true }) mode: "mode-1" | "mode-2" = "mode-1";
+  @Input() additionalClasses: "margin" | "no-margin" = "margin";
   @Input() placeholder = "";
   @Input() text = "";
 }
