cc50e62d94adb29a13c5c9149d212c6973744864
Marcos Segantine <msegantine8@gmail.com>
2024-08-07 11:07:04 -0300

    feat: recreating institutional dashboard frame

--- src/app/features/dashboard/pages/institution-dashboard/institution-dashboard.component.ts
+++ src/app/features/dashboard/pages/institution-dashboard/institution-dashboard.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import { RankingComponent } from '../../../award/ranking/ranking.component';
 
-import { Chart } from 'chart.js';
-
 @Component({
   selector: 'app-institution-dashboard',
   standalone: true,
@@ -13,50 +11,5 @@ import { Chart } from 'chart.js';
 })
 export class InstitutionDashboardComponent implements OnInit {
   ngOnInit(): void {
-    new Chart("frequency", {
-      type: "bar",
-      data: {
-        labels: ["Turma 1", "Turma 2", "Turma 3", "Turma 4"],
-        datasets: [{
-          data: [5, 4, 2, 1],
-          backgroundColor: "#FFB500"
-        }]
-      },
-      options: {
-        responsive: true,
-        plugins: {
-          title: {
-            text: "Frequência",
-            display: true,
-            color: "white",
-            font: {
-              size: 32,
-            },
-            padding: {
-              bottom: 25
-            }
-          },
-          legend: {
-            display: false
-          }
-        }
-      },
-    })
-
-    new Chart("performace", {
-      type: "line",
-      data: {
-        labels: ["Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
-        datasets: [{
-          label: 'My First Dataset',
-          data: [65, 59, 80, 81, 56],
-          fill: true,
-          backgroundColor: "rgba(255, 181, 0, 0.5)",
-          borderColor: '#FFB500',
-          pointRadius: 0,
-          tension: 0.7
-        }]
-      },
-    })
   }
 }
