bf89ac349d08c83c593007db2d7eceeb58d64330
Marcos Segantine <msegantine8@gmail.com>
2024-08-09 19:24:20 -0300

    feat: creating profile student page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,7 @@ import { StudentLessonsComponent } from './features/activities/pages/student-les
 import { StudentLessonComponent } from './features/activities/pages/student-lesson/student-lesson.component';
 import { StudentQuizComponent } from './features/activities/pages/student-quiz/student-quiz.component';
 import { InstitutionDashboardComponent } from './features/dashboard/pages/institution-dashboard/institution-dashboard.component';
+import { StudentProfileComponent } from './features/profiles/student-profile/student-profile.component';
 
 import { MaintenanceComponent } from './core/components/pages/maintenance/maintenance.component';
 import { CoreAppComponent } from './core/components/pages/core-app/core-app.component';
@@ -59,6 +60,10 @@ export const routes: Routes = [
                 {
                     path: "student/activity/lessons/lesson/quiz",
                     component: StudentQuizComponent
+                },
+                {
+                    path: "student/profile",
+                    component: StudentProfileComponent
                 },
                 {
                     path: "professional/home",
