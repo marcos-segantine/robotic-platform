a1ebad7766415eb9e47b200d630e9e3d192f7697
Marcos Segantine <msegantine8@gmail.com>
2024-08-10 15:21:42 -0300

    feat: creating support page

--- src/app/shared/pages/support/support.component.ts
+++ src/app/shared/pages/support/support.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 
+import { InputComponent } from '../../components/input/input.component';
+import { ButtonComponent } from '../../components/button/button.component';
+
+import { ButtonType } from '../../../features/auth/enum/button-type-enum';
+
 @Component({
   selector: 'app-support',
   standalone: true,
-  imports: [],
+  imports: [InputComponent, ButtonComponent],
   templateUrl: './support.component.html',
   styleUrl: './support.component.scss'
 })
 export class SupportComponent {
-
+  buttonType = ButtonType
 }
