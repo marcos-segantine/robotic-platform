61f3346f0d2c2d4cb9927b8baa33c63ada838fcc
Marcos Segantine <msegantine8@gmail.com>
2024-12-15 16:42:02 -0300

    refactor: removing footer because is not using it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,18 @@ import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './core/components/layout/header/header.component';
-import { FooterComponent } from './core/components/layout/footer/footer.component';
-
-import { FooterEnum } from './shared/enums/footer.enum';
 
 import { UserDataService } from './core/services/user-data.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'robotic-platform';
-  footerMode: FooterEnum = FooterEnum.Normal
 
   isLogged = false;
   isStudentOrProfessional = false;
@@ -26,7 +22,6 @@ export class AppComponent {
     this.router.events.subscribe((ev) => {
       if (ev instanceof NavigationEnd) {
         const pathArray = document.location.pathname.split("/")
-        const pagePath = pathArray.at(-1);
 
         if (pathArray.includes("app")) this.isLogged = true;
         else this.isLogged = false;
@@ -39,12 +34,6 @@ export class AppComponent {
         else {
           this.isStudentOrProfessional = false
         }
-
-        if (
-          pagePath == "" ||
-          pagePath == "login"
-        ) this.footerMode = FooterEnum.Normal
-        else this.footerMode = FooterEnum.Hidden
       }
     });
   }
