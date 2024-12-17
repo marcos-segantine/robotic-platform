0354ba70c7849adb60057da35a4d235ab7040562
Marcos Segantine <msegantine8@gmail.com>
2024-12-17 18:04:00 -0300

    feat: updating input component

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,16 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss'
 })
 export class InputComponent {
   @Input({ required: true }) size: "all" | "bigger" | "medium" | "small" = "medium";
   @Input({ required: true }) mode: "mode-1" | "mode-2" = "mode-1";
+  @Input() property = ""; // For now, `property` is not required
   @Input() additionalClasses: "margin" | "no-margin" = "margin";
   @Input() placeholder = "";
   @Input() text = "";
+  @Output() propertyChange = new EventEmitter<string>();
+
+  emitEvent(event: Event) {
+    const input = event.target as HTMLInputElement
+    this.propertyChange.emit(input.value);
+  }
 }
