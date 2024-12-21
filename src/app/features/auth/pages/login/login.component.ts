9c7139e235e3ca0a2dc466be522cbfe88703f86c
Marcos Segantine <msegantine8@gmail.com>
2024-12-20 22:15:44 -0300

    feat: adding explanation to lesson page

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -19,7 +19,7 @@ export class LoginComponent {
 
   login() {
     // after introduce back-end logic, remove this line
-    const userData: UserDataModel | null = this.userDataService.setUserData({ id: "f538dbfb-a6fa-4d97-a1ee-a7af91606261", name: "Marcos", userType: "student" });
+    const userData: UserDataModel | null = this.userDataService.setUserData({ id: "545f2587-dbff-4953-a905-956ef7ce17cf", name: "Marcos", userType: "student" });
 
     if (userData !== null) {
       this.router.navigate([`app/${userData.userType}/home`])
