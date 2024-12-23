7aaad97121214efa17d7b883cd188e6c0f73c999
Marcos Segantine <msegantine8@gmail.com>
2024-12-23 12:15:42 -0300

    fix: fixing ui bug

--- src/app/features/activities/components/statistic/statistic.component.ts
+++ src/app/features/activities/components/statistic/statistic.component.ts
@@ -8,12 +8,14 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrl: './statistic.component.scss'
 })
 export class StatisticComponent implements OnInit {
-  @Input({ required: true }) value = 100;
+  @Input({ required: true }) value = 0;
   @Input({ required: true }) text = "";
 
   color: "red" | "orange" | "green" = "orange";
 
   ngOnInit(): void {
+    this.value = +this.value.toFixed(0);
+    
     if (this.value <= 50) this.color = "red";
     else if (this.value > 90) this.color = "green";
     else this.color = "orange";
