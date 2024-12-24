4081bc43f579e9b696043288f56ac425173c69d2
Marcos Segantine <msegantine8@gmail.com>
2024-12-23 23:50:06 -0300

    fat: adding timer to quiz page

--- src/app/features/activities/pages/student-quiz/student-quiz.component.ts
+++ src/app/features/activities/pages/student-quiz/student-quiz.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
 import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-student-quiz',
@@ -15,12 +16,42 @@ export class StudentQuizComponent implements OnInit {
     question: "",
     alternatives: []
   }
+  timer: number = 120;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private location: Location) {
     this.data.question = this.router.getCurrentNavigation()?.extras?.state?.["data"].question;
     this.data.alternatives = this.router.getCurrentNavigation()?.extras?.state?.["data"].alternatives;
   }
 
   ngOnInit() {
+    if (!this.data.question && !this.data.alternatives) {
+      console.log("redirecting user");
+      this.router.navigate(['app/student/activity']);
+      return;
+    }
+
+    this.updateTimer();
+  }
+
+  async updateTimer() {
+    if (this.timer > 0) {
+      await this.sleep(1000);
+      this.timer--;
+      this.updateTimer();
+    }
+    else {
+      console.log("redirecting user");
+      this.location.back();
+    }
+  }
+
+  sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  convertSecondsToMinutes(seconds: number): string {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes} : ${remainingSeconds < 10 ? '0' + remainingSeconds : remainingSeconds}`;
   }
 }
